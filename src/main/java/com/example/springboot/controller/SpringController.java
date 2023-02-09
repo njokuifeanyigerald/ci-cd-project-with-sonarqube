0fec9184ed531bb4e9f51ef7fc0129a02973015a
CodeByGerald <brainboyrichmond@gmail.com>
2023-02-09 12:50:49 +0100

    done

--- src/main/java/com/example/springboot/controller/SpringController.java
+++ src/main/java/com/example/springboot/controller/SpringController.java
@@ -23,7 +23,7 @@ public class SpringController {
 //		Integer sec = date.getSecond();
 		val++;
 //		Integer count = year + month + day + hour + min + sec + 1;
-		String result = "Mr. DevOps" + val;
+		String result = "Njoku Ifeanyi Gerald" + val;
 		return result;
 	}
 }
